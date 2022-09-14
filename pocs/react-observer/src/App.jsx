711348d12e3a19e7d6cde1a3d1833dc10a6fea4e
mateuzor <mateuzor@gmail.com>
2022-09-13 23:00:03 -0300

    react observer initial project

---
+++ pocs/react-observer/src/App.jsx
@@ -0,0 +1,39 @@
+import "./App.css";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+function App() {
+  const [allPokemons, setAllPokemons] = useState([]);
+  useEffect(() => {
+    async function getPokemons() {
+      const { data } = await axios.get(
+        "https://pokeapi.co/api/v2/pokemon?limit=799&offset=100"
+      );
+      if (data.results) {
+        const pokemons = data.results.map((pokemon, index) => ({
+          name: pokemon.name,
+          id: index + 100,
+        }));
+        setAllPokemons(pokemons);
+      }
+    }
+    getPokemons();
+  }, []);
+  return (
+    <div className="App">
+      {allPokemons.map(({ name, id }, index) => {
+        return (
+          <div key={name}>
+            <h2>{name}</h2>
+            <img
+              src={`https://raw.githubusercontent.com/pokeapi/sprites/master/sprites/pokemon/${id}.png`}
+              alt={name}
+            />
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
+export default App;
