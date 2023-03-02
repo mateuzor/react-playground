776f55427af45d55502eea1efe4e4af2bf02fb99
Mateus Ramos Ferreira <mateuzor@Mateuss-MacBook-Pro.local>
2023-03-02 09:48:37 -0300

    astro project

---
+++ pocs/astro/src/components/Button.jsx
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+
+const Button = () => {
+  const [num, setNum] = useState(0);
+  return (
+    <button onClick={() => setNum((prev) => prev + 1)}>Button {num}</button>
+  );
+};
+
+export default Button;
