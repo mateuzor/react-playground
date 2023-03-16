155d290cfcd81e9a73b33d47843d30c31514a2c9
Mateus Ramos Ferreira <mateuzor@Mateuss-MacBook-Pro.local>
2023-03-16 07:30:07 -0300

    finished progress bar

--- pocs/spotify-clone/src/components/PlayerControls/index.jsx
+++ pocs/spotify-clone/src/components/PlayerControls/index.jsx
@@ -10,14 +10,53 @@ import { CgPlayTrackNext, CgPlayTrackPrev } from "react-icons/cg";
 import { FiRepeat } from "react-icons/fi";
 export default function PlayerControls() {
   const audiotagRef = useRef(null);
-  const [playerState, setPlayerState] = useState("pause");
+  const progressBarRef = useRef(null);
+  const [playerState, setPlayerState] = useState("play");
+  const [musicCurrentTime, setMusicCurrentTime] = useState(null);
+  const [musicDuration, setMusicDuration] = useState(null);
+  const [duratioInSeconds, setDuratioInSeconds] = useState(null);
+
+  const [state, setSlide] = useState(0);
+
+  const calculateMusicLevel = (event) => {
+    const result = (event.target.value / 100) * duratioInSeconds;
+    audiotagRef.current.currentTime = result;
+    calculateTime(result, "currentTime");
+  };
+
+  const handleChange = (e) => {
+    if (e.target.value === "100") {
+      setPlayerState("play");
+      setMusicCurrentTime(null);
+      setMusicDuration(null);
+      setSlide(0);
+    }
+    if (e.target.value === 0) {
+      setPlayerState("pause");
+    }
+    calculateMusicLevel(e);
+  };
+
+  const handleProgressBar = (currentTime, duration) => {
+    const result = (currentTime / duration) * 100;
+    if (result === 100) {
+      setPlayerState("play");
+      audiotagRef.current.currentTime = 0;
+      progressBarRef.current.value = 0;
+      setMusicCurrentTime(null);
+      setMusicDuration(null);
+    }
+    progressBarRef.current.value = result;
+  };
+
+  let mouseDownOnSlider = false;
 
   const changeState = () => {
     if (playerState === "pause") {
-      audiotagRef.current.play();
+      audiotagRef.current.pause();
       setPlayerState("play");
     } else {
-      audiotagRef.current.pause();
+      audiotagRef.current.play();
       setPlayerState("pause");
     }
   };
@@ -26,15 +65,39 @@ export default function PlayerControls() {
 
   const getAudio = async () => {
     try {
-      const result = await axios.get("http://localhost:3001/play-audio");
+      const result = await axios.get("http://localhost:3001/playlist");
       console.log(result);
-    } catch (error) {
-      console.log(error);
-    }
+    } catch (error) {}
   };
 
   React.useEffect(() => {
     getAudio();
+  }, []);
+
+  const calculateTime = (secs, type) => {
+    const minutes = Math.floor(secs / 60);
+    const seconds = Math.floor(secs % 60);
+    const returnedSeconds = seconds < 10 ? `0${seconds}` : `${seconds}`;
+    if (type === "duration") {
+      setMusicDuration(`${minutes}:${returnedSeconds}`);
+      setDuratioInSeconds(secs);
+    } else {
+      setMusicCurrentTime(`${minutes}:${returnedSeconds}`);
+    }
+  };
+
+  React.useEffect(() => {
+    if (audiotagRef) {
+      audiotagRef.current.addEventListener("timeupdate", (e) => {
+        const { duration, currentTime } = e.target;
+        handleProgressBar(currentTime, duration);
+        calculateTime(duration, "duration");
+        calculateTime(currentTime, "currentTime");
+      });
+      return () => {
+        audiotagRef.current.removeEventListener("timeupdate", () => {});
+      };
+    }
   }, []);
 
   const handleAudioRepeat = () => {
@@ -47,31 +110,83 @@ export default function PlayerControls() {
 
   return (
     <Container>
-      <div className="shuffle">
-        <BsShuffle />
-      </div>
-      <div className="previous">
-        <CgPlayTrackPrev onClick={() => changeTrack("previous")} />
-      </div>
-      <div className="state">
+      <CurrentSoundContainer>
+        <img
+          height={80}
+          src="https://www.soundhelix.com/sites/default/files/SoundHelix-logo-medium.png"
+        />
         <div>
-          {playerState === "pause" ? (
-            <BsFillPauseCircleFill onClick={changeState} />
-          ) : (
-            <BsFillPlayCircleFill onClick={changeState} />
-          )}
+          <p className="sound-name">Song-01</p>
+          <p className="band-name">SoundHelix</p>
+        </div>
+      </CurrentSoundContainer>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "cemter",
+        }}
+      >
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-around",
+            alignItems: "center",
+          }}
+        >
+          <div className="shuffle">
+            <BsShuffle />
+          </div>
+          <div className="previous">
+            <CgPlayTrackPrev onClick={() => changeTrack("previous")} />
+          </div>
+          <div className="state">
+            <div>
+              {playerState === "pause" ? (
+                <BsFillPauseCircleFill onClick={changeState} />
+              ) : (
+                <BsFillPlayCircleFill onClick={changeState} />
+              )}
+            </div>
+          </div>
+          <div className="next">
+            <CgPlayTrackNext onClick={() => changeTrack("next")} />
+          </div>
+          <div className="repeat">
+            <FiRepeat onClick={handleAudioRepeat} />
+          </div>
+        </div>
+
+        <div style={{ display: "flex", alignItems: "center" }}>
+          <p style={{ color: "white", marginRight: "10px", fontSize: "11px" }}>
+            {musicCurrentTime}
+          </p>
           <audio
+            style={{ margin: "0 10px" }}
             hidden
+            controls
+            controlsList="nodownload noplaybackrate"
             ref={audiotagRef}
             src="http://localhost:3001/playlist"
           />
+          <input
+            ref={progressBarRef}
+            style={{ width: "400px" }}
+            type="range"
+            id="threshold"
+            min={0}
+            max={100}
+            step={0.5}
+            defaultValue={state}
+            onMouseUp={handleChange}
+          />
+          {musicDuration && (
+            <p style={{ color: "white", marginLeft: "10px", fontSize: "11px" }}>
+              {musicDuration}
+            </p>
+          )}
         </div>
-      </div>
-      <div className="next">
-        <CgPlayTrackNext onClick={() => changeTrack("next")} />
-      </div>
-      <div className="repeat">
-        <FiRepeat onClick={handleAudioRepeat} />
       </div>
       <VolumeContainer>
         <input
@@ -89,8 +204,9 @@ export default function PlayerControls() {
 
 const Container = styled.div`
   display: flex;
+  width: 100vw;
   align-items: center;
-  justify-content: center;
+  justify-content: space-around;
   gap: 2rem;
   svg {
     color: #b3b3b3;
@@ -108,6 +224,29 @@ const Container = styled.div`
   .next,
   .state {
     font-size: 2rem;
+  }
+`;
+
+const CurrentSoundContainer = styled.div`
+  display: flex;
+  align-items: center;
+  img {
+    height: 56px;
+    margin-right: 15px;
+  }
+  div {
+    display: flex;
+    flex-direction: column;
+    .sound-name {
+      font-size: 14px;
+      margin: 0;
+      color: white;
+    }
+    .band-name {
+      font-size: 11px;
+      margin: 0;
+      color: white;
+    }
   }
 `;
 
