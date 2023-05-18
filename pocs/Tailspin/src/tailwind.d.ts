ab7b025e8e639453b348157312609659654d664a
Mateus Ramos Ferreira <mateuzor@Mateuss-MacBook-Pro.local>
2023-05-18 09:12:05 -0300

    watcher ready, missing autocompletion

---
+++ pocs/Tailspin/src/tailwind.d.ts
@@ -0,0 +1,31 @@
+declare const styles: {
+  // Colors
+  "bg-primary": string;
+  "bg-secondary": string;
+  "bg-success": string;
+  "bg-info": string;
+  "bg-warning": string;
+  "bg-danger": string;
+  "bg-light": string;
+  "bg-dark": string;
+
+  "text-primary": string;
+  "text-secondary": string;
+  "text-success": string;
+  "text-info": string;
+  "text-warning": string;
+  "text-danger": string;
+  "text-light": string;
+  "text-dark": string;
+
+  // Spacing
+  "m-1": string;
+  "m-2": string;
+  "m-3": string;
+  "m-4": string;
+  "m-5": string;
+
+  "p-1": string;
+  "p-2": string;
+  "p-3": string;
+};
