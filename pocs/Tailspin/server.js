de2daae1086427c93630ce285859c9d4a43eaa79
Mateus Ramos Ferreira <mateuzor@Mateuss-MacBook-Pro.local>
2023-05-31 22:07:23 -0300

    fixed the autoreload and created a basic layout using my classes

--- pocs/Tailspin/server.js
+++ pocs/Tailspin/server.js
@@ -1,56 +1,51 @@
-const express = require("express");
-const http = require("http");
-const socketIo = require("socket.io");
 const chokidar = require("chokidar");
-const fs = require("fs");
+const { exec } = require("child_process");
+const express = require("express");
 const path = require("path");
 
 const app = express();
-const server = http.createServer(app);
-const io = socketIo(server);
-
-const htmlFilePath = "index.html";
 const distPath = path.join(__dirname, "dist");
 
 // Serve the static files from the dist directory
 app.use(express.static(distPath));
 
-// Serve the tailwind.css file with the correct MIME type
-app.get("/tailwind.css", (req, res) => {
-  res.setHeader("Content-Type", "text/css");
-  res.sendFile(path.join(distPath, "tailwind.css"));
+// Route for serving the index.html file
+app.get("/", (req, res) => {
+  res.sendFile(path.join(distPath, "index.html"));
 });
 
-// Configure the WebSocket server
+// Watch for file changes in the src directory
+const watcher = chokidar.watch(path.join(__dirname, "src"));
+
+watcher.on("change", () => {
+  console.log("HTML file changed. Reloading...");
+  exec("npm run build", (error, stdout, stderr) => {
+    if (error) {
+      console.error(`Error: ${error.message}`);
+      return;
+    }
+    if (stderr) {
+      console.error(`stderr: ${stderr}`);
+      return;
+    }
+    console.log(stdout);
+    io.emit("reload");
+  });
+});
+
+// Start the server
+const port = 3000;
+const server = app.listen(port, () => {
+  console.log(`Server started at http://localhost:${port}`);
+});
+
+// Set up Socket.IO
+const io = require("socket.io")(server);
+
 io.on("connection", (socket) => {
   console.log("Client connected.");
 
-  // Monitor changes to the HTML file
-  const watcher = chokidar.watch(htmlFilePath);
-  watcher.on("change", () => {
-    console.log("The HTML file has changed.");
-
-    // Read the updated HTML file content
-    fs.readFile(htmlFilePath, "utf8", (err, data) => {
-      if (err) {
-        console.error("Error reading the HTML file:", err);
-        return;
-      }
-
-      // Send a reload signal to the client
-      socket.emit("reload");
-    });
-  });
-
-  // Handle client disconnection
   socket.on("disconnect", () => {
     console.log("Client disconnected.");
-    watcher.close();
   });
-});
-
-// Start the HTTP server
-const port = 3000;
-server.listen(port, () => {
-  console.log(`Server started at http://localhost:${port}`);
 });
